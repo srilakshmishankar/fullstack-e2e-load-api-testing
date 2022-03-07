89ed3e1274faf60fa79291f13219dcafe8a27662
Srilakshmi Shankar <sshankar@surveymonkey.com>
2022-03-07 10:05:08 +0100

    load tests for create session API

---
+++ load-tests/create-session.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+
+export const options = {
+    stages: [
+        { duration: '5s', target: 10 },
+        { duration: '5s', target: 50 },
+        { duration: '5s', target: 100 },
+        { duration: '5s', target: 50 },
+        { duration: '5s', target: 0 },
+    ],
+};
+
+const url = 'http://host.docker.internal:3001/api/sessions';
+const payload = JSON.stringify({
+    name: 'Load testing',
+    time: 10000,
+    createdAt: new Date().toString(),
+});
+
+const params = {
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+export default function () {
+    http.post(url, payload, params);
+}
